a1c6d4437aadd673279d851a8c74ec7e772c29a3
filmlogica <filmlogica@gmail.com>
2025-06-14 17:11:03 -0400

    Create DownloadPage.js

---
+++ frontend/src/pages/DownloadPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DownloadPage = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Download Your Products</h1>
+      <p>Access your purchased files here. Stay tuned for updates!</p>
+    </div>
+  );
+};
+
+export default DownloadPage;
