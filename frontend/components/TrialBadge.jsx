40667ac3ffc3f1d44063699ad76363176e79d81f
filmlogica <filmlogica@gmail.com>
2025-06-14 10:43:22 -0400

    Create TrialBadge.jsx

---
+++ frontend/components/TrialBadge.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TrialBadge = () => (
+  <div className="trial-badge">
+    <p>🔥 7-Day Free Trial Available on All Plans!</p>
+  </div>
+);
+
+export default TrialBadge;
