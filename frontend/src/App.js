27bdc294a2807ff51a6e88651eec1b2feda0cd14
filmlogica <filmlogica@gmail.com>
2025-06-14 21:32:49 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import AffiliatePage from "./pages/AffiliatePage";
+import AffiliatePage from "./pages/AffiliateSignup";
 import CheckoutPage from "./pages/CheckoutPage";
 import LandingPage from "./pages/LandingPage";
 import PricingPage from "./pages/PricingPage";
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/pricing" element={<PricingPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/affiliate" element={<AffiliatePage />} />
+        <Route path="/affiliate" element={<AffiliateSignup />} />
         <Route path="/download" element={<DownloadPage />} />
       </Routes>
     </div>
